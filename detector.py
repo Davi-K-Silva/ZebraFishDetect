d5f2cfd496cf66d831e45af28b4b35c47869d060
renan <renan262001@gmail.com>
2021-04-16 17:08:52 -0300

    frame de fundo fixo definido

--- detector.py
+++ detector.py
@@ -9,18 +9,8 @@ color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
 # Open Video
 cap = cv2.VideoCapture('video1.avi')
 
-# Randomly select 25 frames
-frameIds = cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25)
-
-# Store selected frames in an array
-frames = []
-for fid in frameIds:
-    cap.set(cv2.CAP_PROP_POS_FRAMES, fid)
-    ret, frame = cap.read()
-    frames.append(frame)
-
-# Calculate the median along the time axis
-medianFrame = np.median(frames, axis=0).astype(dtype=np.uint8)    
+# Set median frame
+medianFrame = cv2.imread('BaseBkg.jpg')
 
 # Display median frame
 cv2.imshow('Median Background Frame', medianFrame)
