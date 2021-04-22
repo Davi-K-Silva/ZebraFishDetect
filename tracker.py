84974c3dd1967b7a219a4678722f7fba862475af
EduardoSpode <Eduardo.Spode01@edu.pucrs.b>
2021-04-22 16:11:07 -0300

    Add comentários

--- tracker.py
+++ tracker.py
@@ -18,7 +18,7 @@ class EuclideanDistTracker:
             cx = (x + x + w) // 2      
             cy = (y + y + h) // 2      
 
-            possiv_ids = [1,2,3,4,5,6,7,8,9,10,11]
+            possiv_ids = [1,2,3,4,5,6,7,8,9,10,11] #Lista com os possíveis Ids
 
             # Find out if that object was detected already
             same_object_detected = False
@@ -54,24 +54,24 @@ class EuclideanDistTracker:
         # Update dictionary with IDs not used removed
         self.center_points = new_center_points.copy()
 
-        dic = {}
-        for x in objects_bbs_ids:
+        dic = {} #Cria um dicionário
+        for x in objects_bbs_ids: #Adiciona um lista no dionário baseado no id presente na lista
             dic[x[4]] = x
 
-        objects_bbs_ids.clear()
+        objects_bbs_ids.clear() # Limpa a lista
 
-        for i in range(1,9):
-            if i in dic.keys():
+        for i in range(1,9): #Verifica se há algum id faltando de 1 a 8  e se estiver faltando adiciona uma lista no id faltando
+            if i in dic.keys(): 
                 continue
             else:
                 dic[i] = [1,1,1,1,0]
 
-        for key in sorted(dic.keys()) :
+        for key in sorted(dic.keys()) : # Ordena o dionário baseado nos ids adionando-os em uma lista
             objects_bbs_ids.append(dic[key])
 
         lista = []
         count = 0
-        for x in objects_bbs_ids:
+        for x in objects_bbs_ids: # Separa os 8 primeiros elementos da lista e cria uma nova
             if count <= 7:
                 lista.append(x)
                 count += 1
@@ -79,6 +79,5 @@ class EuclideanDistTracker:
                 break
 
         return lista
-
 
 
