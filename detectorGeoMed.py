cbd5a5645a7d32ad5fa3fdecb3805aac043c9a51
Davi-K-Silva <davi.KDS.01@gmail.com>
2021-04-23 09:46:33 -0300

    Mediana Geométrica atualizada

--- detectorGeoMed.py
+++ detectorGeoMed.py
@@ -1,7 +1,10 @@
+import math
 import numpy as np
+import random as rng
 from cv2 import cv2
 from skimage import data, filters
-from tracker import  EuclideanDistTracker, GeoMTracker
+from Leitura import Leitura
+from tracker import *
 from scipy.spatial.distance import cdist, euclidean
 
 def geometric_median(X, eps=1e-5):
@@ -32,93 +35,146 @@ def geometric_median(X, eps=1e-5):
 
         y = y1
 
-#
-Colors = [[255,0,0],[0,255,0],[0,0,255],
-          [255,255,0],[255,0,255],[0,255,255],
-          [128,0,255],[0,128,255],[255,0,128],
-          [255,128,0]]
+color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
 
 # Open Video
-cap = cv2.VideoCapture('Video1.avi')
+cap = cv2.VideoCapture('video1.avi')
+
 
 #Create tracker object
-Gtracker = GeoMTracker()
+tracker = GeoMEuclideanDistTracker()
 
-medianFrame = cv2.imread("BaseBkg.jpg")    
+# Set median frame
+medianFrame = cv2.imread('BaseBkg.jpg')
 
 # Display median frame
-cv2.imshow('frame', medianFrame)
+cv2.imshow('Median Background Frame', medianFrame)
 cv2.waitKey(0)
-cv2.imwrite("BaseBkg.jpg", medianFrame)
+
 # Reset frame number to 0
 cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
 # Convert background to grayscale
 grayMedianFrame = cv2.cvtColor(medianFrame, cv2.COLOR_BGR2GRAY)
+
+# Variaveis do leitor 
+countVet = 0
+vet = Leitura().leitura()
+""" arq = Leitura() """
+
+countFrame = 1
 # Loop over all frames
 ret = True
-hasbeenrun = False
-mask = np.zeros(grayMedianFrame.shape, dtype=np.uint8)
 
+mask = np.zeros(grayMedianFrame.shape, dtype=np.uint8)
 while(ret):
 
   # Read frame
   ret, frame = cap.read()
   # Convert current frame to grayscale
-  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  gframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
   # Calculate absolute difference of current frame and 
   # the median frame
-  dframe = cv2.absdiff(frame, grayMedianFrame)
+  dframe = cv2.absdiff(gframe, grayMedianFrame)
   # Treshold to binarize
-  
   th, dframe = cv2.threshold(dframe, 30, 255, cv2.THRESH_BINARY)
-
+  # th = cv2.dilate(th, None, iterations=2)
+  cnts,hierarchy = cv2.findContours(dframe, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
   
-  # Perform the distance transform algorithm
-  dist = cv2.distanceTransform(dframe, cv2.DIST_L2, 3)
-  # Normalize the distance image for range = {0.0, 1.0}
-  # so we can visualize and threshold it
-  cv2.normalize(dist, dist, 0, 1.0, cv2.NORM_MINMAX)
+  i = -1
+
+  detections = []
+  
+  # loop over the contours
+  for c in cnts:
+    i = i + 1
+    # if the contour is too small, ignore it
+    if cv2.contourArea(c) < 50:
+      continue
+
+    cv2.drawContours(mask,[c], -1, (255,255,255),-1)
+    coords = np.column_stack(np.where(mask>0))
+    geoM = geometric_median(coords)
+
+    mask = np.zeros(frame.shape, dtype=np.uint8)
+        
+    x = int(geoM[1])
+    y = int(geoM[0])
+    detections.append([x,y])
+
+    cord = "x:" + str(x) + "y:" + str(y)
+
+
+    cv2.putText(frame, cord, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+    cv2.drawContours(frame, cnts, i, color, 2, cv2.LINE_8, hierarchy, 0)
+
+  fishes_ids = tracker.update(detections) 
+  print(countFrame)
+  
+  """ for fish_id in fishes_ids:
+    x,y,w,h,id_ = fish_id
+    
+     with open("peixe.txt",'a') as arquivo:                       
+            arquivo.write(str(x + w/2)+";"+str(y + h/2)+";" + "  |  "+ str(id_)+" |  ") 
+     
+    with open("peixe.txt",'a') as arquivo:                       
+            arquivo.write(str(x + w/2)+";"+str(y + h/2)+";")
+   
+  with open("peixe.txt",'a') as arquivo:                        
+            arquivo.write("\n") """
+
+
+  countFrame = countFrame + 1
+  
+  length = len(vet[countVet])/2
+  
+  
+  for i in range(int(length)):
+    if i == 0:
+      vetX = int(vet[countVet][0])
+      vetY = int(vet[countVet][1])
+    else:
+      vetX = int(vet[countVet][i*2])
+      vetY = int(vet[countVet][i*2+1])
+
+    if i == 0:
+      if vetX & vetX != 1:
+        cv2.circle(frame,(vetX,vetY),5,(0,0,255), -1)   # Vermelho  ID = 1
+    elif i == 1:
+      if vetX & vetX != 1:
+        cv2.circle(frame,(vetX,vetY),5,(0,255,0), -1)   # Verde     ID = 2
+    elif i == 2:
+      if vetX & vetX != 1:
+        cv2.circle(frame,(vetX,vetY),5,(255,0,0), -1)   #Azul       ID = 3
+    elif i == 3:
+      if vetX & vetX != 1:
+        cv2.circle(frame,(vetX,vetY),5,(255,0,255), -1) #Roxo       ID = 4
+    elif i == 4:
+      if vetX & vetX != 1:
+        cv2.circle(frame,(vetX,vetY),5,(255,255,0), -1) #Azul claro ID = 5
+    elif i == 5:
+      if vetX & vetX != 1:
+        cv2.circle(frame,(vetX,vetY),5,(0,255,255), -1) #Amarelo    ID = 6
+    elif i == 6:
+      if vetX & vetX != 1:
+        cv2.circle(frame,(vetX,vetY),5,(120,150,0), -1) #Ciano      ID = 7
+    elif i == 7:
+      if vetX & vetX != 1:
+        cv2.circle(frame,(vetX,vetY),5,(60,70,190), -1) #Marrom     ID = 8
  
   
-  contours, _ = cv2.findContours(dframe, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-  detections = []
 
-  for cnt in contours:
-      area = cv2.contourArea(cnt)
-      if area > 70:
-        cv2.drawContours(frame,[cnt], -1, (0,255,50),2)
-        cv2.drawContours(mask,[cnt], -1, (255,255,255),-1)
-        coords = np.column_stack(np.where(mask>0))
-        geoM = geometric_median(coords)
-        
-        mask = np.zeros(frame.shape, dtype=np.uint8)
-        
-        detections.append([int(geoM[1]),int(geoM[0])])
-
-  if not hasbeenrun:
-    Gtracker.start(detections)
-    hasbeenrun = True
-
-  fishes_ids = Gtracker.update(detections)
-  
-  for fish_id in fishes_ids:
-    cx,cy,id_ = fish_id
-    cv2.putText(frame, "id: "+ str(id_), (cx,cy -15), cv2.FONT_HERSHEY_PLAIN,1,(255,0,0),2)
-    print(str(cx)+","+str(cy),end=";")
-     
-   
   # Display image
-  cv2.imshow('frame', dframe)
-  cv2.imshow("Frame" , frame)
+  cv2.imshow('Background Subtraction', dframe)
+  cv2.imshow('Detection', frame)
 
-  print("")
+  #Count for file
+  countVet = countVet + 1
 
-  key = cv2.waitKey(0)
-
+  key = cv2.waitKey(50)
+  
   if key == 27:
       break
-
 # Release video object
 cap.release()
 
--- tracker.py
+++ tracker.py
@@ -80,4 +80,81 @@ class EuclideanDistTracker:
 
         return lista
 
+class GeoMEuclideanDistTracker:
+    def __init__(self):
+        # Store the center positions of the objects
+        self.center_points = {}
+        # Keep the count of the IDs
+        
+
+    def update(self, objects_cord):
+        # Objects boxes and ids
+        objects_bbs_ids = []    
+        
+        # Get center point of new object
+        for cord in objects_cord:
+            cx = cord[0]
+            cy = cord[1]     
+
+            possiv_ids = [1,2,3,4,5,6,7,8,9,10,11] #Lista com os possíveis Ids
+
+            # Find out if that object was detected already
+            same_object_detected = False
+            
+            for id, pt in self.center_points.items():
+                dist = math.hypot(cx - pt[0], cy - pt[1])
+                
+                if id in possiv_ids:
+                    del(possiv_ids[possiv_ids.index(id)]) # Se um peixe já existir com o Id, então retira da lista de possíveis Ids
+                if dist < 25:
+                    self.center_points[id] = (cx, cy)
+                    
+                    objects_bbs_ids.append([cx, cy, id])
+                    same_object_detected = True
+                    break
+
+            # New object is detected we assign the ID to that object
+            if same_object_detected is False:
+                
+                id_ = possiv_ids[0] #Adiciona o Id que está em primeiro na lista de possíveis Ids
+
+                self.center_points[id_] = (cx, cy)
+                objects_bbs_ids.append([cx, cy, id_])
+                
+
+        # Clean the dictionary by center points to remove IDS not used anymore
+        new_center_points = {}
+        for obj_bb_id in objects_bbs_ids:
+            _, _, object_id = obj_bb_id
+            center = self.center_points[object_id]
+            new_center_points[object_id] = center
+
+        # Update dictionary with IDs not used removed
+        self.center_points = new_center_points.copy()
+
+        dic = {} #Cria um dicionário
+        for x in objects_bbs_ids: #Adiciona um lista no dionário baseado no id presente na lista
+            dic[x[2]] = x
+
+        objects_bbs_ids.clear() # Limpa a lista
+
+        for i in range(1,9): #Verifica se há algum id faltando de 1 a 8  e se estiver faltando adiciona uma lista no id faltando
+            if i in dic.keys(): 
+                continue
+            else:
+                dic[i] = [1,1,1,1,0]
+
+        for key in sorted(dic.keys()) : # Ordena o dionário baseado nos ids adionando-os em uma lista
+            objects_bbs_ids.append(dic[key])
+
+        lista = []
+        count = 0
+        for x in objects_bbs_ids: # Separa os 8 primeiros elementos da lista e cria uma nova
+            if count <= 7:
+                lista.append(x)
+                count += 1
+            else:
+                break
+
+        return lista
 
